fa71630abf64a13c49001ab3852e7f9379c44c61
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2021-11-26 15:57:23 +0100

    Update dependencies

--- cmake/pcapng_exporter.cmake
+++ cmake/pcapng_exporter.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
   pcapng_exporter
   GIT_REPOSITORY    https://github.com/Technica-Engineering/Technica.Traces.Pcapng.Exporter.git
-  GIT_TAG           724613f
+  GIT_TAG           2d2c5e7
 )
 
 FetchContent_MakeAvailable(pcapng_exporter)
