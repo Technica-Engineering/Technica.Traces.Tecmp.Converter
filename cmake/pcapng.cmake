a98d787ada650515f854141128f24bf036171369
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2022-06-22 11:35:22 +0200

    Upgrade  LightPcapNg (#19)

--- cmake/pcapng.cmake
+++ cmake/pcapng.cmake
@@ -5,6 +5,6 @@ include(FetchContent)
 FetchContent_Declare(
   pcapng
   GIT_REPOSITORY    https://github.com/Technica-Engineering/LightPcapNg.git
-  GIT_TAG           68a8bef
+  GIT_TAG           990b0bf
 )
 FetchContent_MakeAvailable(pcapng)
