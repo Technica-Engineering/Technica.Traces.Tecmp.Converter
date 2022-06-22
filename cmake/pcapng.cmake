615c64c0a8b9222a2176044da69bb567530610eb
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2022-06-22 15:51:47 +0200

    Fix timestamp writing

--- cmake/pcapng.cmake
+++ cmake/pcapng.cmake
@@ -5,6 +5,6 @@ include(FetchContent)
 FetchContent_Declare(
   pcapng
   GIT_REPOSITORY    https://github.com/Technica-Engineering/LightPcapNg.git
-  GIT_TAG           990b0bf
+  GIT_TAG           c8e5657
 )
 FetchContent_MakeAvailable(pcapng)
