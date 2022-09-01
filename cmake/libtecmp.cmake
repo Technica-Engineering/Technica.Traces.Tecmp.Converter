2641355acfe5cb7e184a6d12d3f9087db54c0b63
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2022-09-01 10:11:01 +0200

    Test VLAN skipping
    Test https://github.com/Technica-Engineering/libtecmp/pull/6

--- cmake/libtecmp.cmake
+++ cmake/libtecmp.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
   libtecmp
   GIT_REPOSITORY    https://github.com/Technica-Engineering/libtecmp.git
-  GIT_TAG           629c9e9
+  GIT_TAG           85dd8b8
 )
 
 FetchContent_MakeAvailable(libtecmp)
