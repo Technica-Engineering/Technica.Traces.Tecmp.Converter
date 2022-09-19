629d092c77f74d5c5afc59bed73df376ba0a1c40
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2022-09-19 16:34:44 +0200

    Update cmake/libtecmp.cmake

--- cmake/libtecmp.cmake
+++ cmake/libtecmp.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
   libtecmp
   GIT_REPOSITORY    https://github.com/aamereller/libtecmp.git
-  GIT_TAG           0517586
+  GIT_TAG           917608e
 )
 
 FetchContent_MakeAvailable(libtecmp)
