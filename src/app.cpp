a24962a7ea4ad90f503c84e445cbc474fe65172f
Andreas Amereller <andreas.amereller@technica-engineering.de>
2023-11-30 13:03:53 +0100

    Added BRS switch.

--- src/app.cpp
+++ src/app.cpp
@@ -93,6 +93,7 @@ void transform(
 				if (header.data_type == TECMP_DATA_CANFD)
 				{
 					can.flags |= CANFD_FDF;
+					can.flags |= data[5] & CANFD_BRS
 					can.flags |= header.data_flags & TMP_ERROR_NODE_ACTIVE ? CANFD_ESI : 0;
 				}
 				memcpy(can.data, data + 5, can.len);
