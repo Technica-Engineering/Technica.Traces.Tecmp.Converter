a1904ab456856cd020a891689e4db0ce38e77c19
RayanMRABET <112360249+RayanMRABET@users.noreply.github.com>
2024-10-16 07:35:30 +0100

    ANDI-10609 : Correct CAN-FD error frame conversion (#37)
    ---------
    
    Co-authored-by: Andreas Amereller <andreas.amereller@technica-engineering.de>
    Co-authored-by: Rayan MRABET <rayan.mrabet@technica-engineering.de>

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) 2020-2021 Technica Engineering GmbH
+	Copyright (c) 2020-2024 Technica Engineering GmbH
 	GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 */
 
@@ -22,7 +22,16 @@
 // TECMP documentation a temporary variable has been generated
 // to provide a temporary logic.
 #define TMP_ERROR_NODE_ACTIVE 0x0002
+#define TMP_ERROR_MESSAGE 0x0008
 #define TMP_BITRATE_SWITCH 0x0010
+// The following fields must be shifted by 1 bit in CAN-FD frames
+#define TMP_BITSTUFF_ERROR 0x0010
+#define TMP_CRC_DEL_ERROR 0x0020
+#define TMP_ACK_DEL_ERROR 0x0040
+#define TMP_EOF_ERROR 0x0080
+// No more bitshifting required
+#define TMP_CRC_ERROR 0x2000
+#define TMP_OVERFLOW 0x8000
 #define NANOS_PER_SEC 1000000000
 
 #define PCAP_NG_MAGIC_NUMBER 0x0A0D0D0A
@@ -52,6 +61,20 @@ bool LoadNpcapDlls()
 	return true;
 }
 #endif
+
+static uint32_t get_can_error_id(uint8_t* data, uint16_t flags, bool is_canfd) {
+	uint32_t can_id = 0x20000000;
+	int bitshift = is_canfd ? 1 : 0;
+	data[2] |= (flags & (TMP_BITSTUFF_ERROR << bitshift)) != 0 ? 0x04 : 0;
+	data[3] |= (flags & TMP_CRC_ERROR) != 0 ? 0x08 : 0;
+	data[3] |= (flags & (TMP_CRC_DEL_ERROR << bitshift)) != 0 ? 0x18 : 0;
+	data[3] |= (flags & (TMP_ACK_DEL_ERROR << bitshift)) != 0 ? 0x1B : 0;
+	data[3] |= (flags & (TMP_EOF_ERROR << bitshift)) != 0 ? 0x1A : 0;
+	if ((data[2] != 0) || (data[3] != 0)) {
+		can_id |= 0x00000008;
+	}
+	return can_id;
+}
 
 void transform(
 	PcapngExporter exporter,
@@ -94,14 +117,23 @@ void transform(
 		if (header.data_type == TECMP_DATA_CAN || header.data_type == TECMP_DATA_CANFD) {
 			struct canfd_frame can = { 0 };
 			can.can_id = ntoh32(*((uint32_t*)data));
-			can.len = data[4];
-			if (header.data_type == TECMP_DATA_CANFD)
+			// Initialize data
+			memset(can.data, 0, sizeof(can.data));
+			if ((header.data_flags & (TMP_ERROR_MESSAGE | TMP_BITSTUFF_ERROR |
+				TMP_ACK_DEL_ERROR | TMP_CRC_DEL_ERROR | TMP_CRC_ERROR | TMP_EOF_ERROR)) != 0) {
+				can.len = 8;
+				can.can_id = get_can_error_id(can.data, header.data_flags, header.data_type == TECMP_DATA_CANFD);
+			}
+			else if (header.data_type == TECMP_DATA_CANFD)
 			{
 				can.flags |= CANFD_FDF;
 				can.flags |= header.data_flags & TMP_BITRATE_SWITCH ? CANFD_BRS : 0;
 				can.flags |= header.data_flags & TMP_ERROR_NODE_ACTIVE ? CANFD_ESI : 0;
 			}
-			memcpy(can.data, data + 5, can.len);
+			else {
+				can.len = data[4];
+				memcpy(can.data, data + 5, can.len);
+			}
 			exporter.write_can(hdr, can);
 		}
 		else if (header.data_type == TECMP_DATA_LIN)
