449f03ab0d56966c73b1302afbe0abba3d82fdb3
Rayan MRABET <rayan.mrabet@technica-engineering.de>
2024-10-30 12:33:50 +0100

    Update app.cpp
    fix regression on converting can-fd packets with removing encapsulation

--- src/app.cpp
+++ src/app.cpp
@@ -119,8 +119,13 @@ void transform(
 			can.can_id = ntoh32(*((uint32_t*)data));
 			// Initialize data
 			memset(can.data, 0, sizeof(can.data));
-			if ((header.data_flags & (TMP_ERROR_MESSAGE | TMP_BITSTUFF_ERROR |
-				TMP_ACK_DEL_ERROR | TMP_CRC_DEL_ERROR | TMP_CRC_ERROR | TMP_EOF_ERROR)) != 0) {
+
+			int bitshift = (header.data_type == TECMP_DATA_CANFD) ? 1 : 0;
+			bool has_error = ((header.data_flags & (TMP_ERROR_MESSAGE | TMP_BITSTUFF_ERROR << bitshift |
+				TMP_ACK_DEL_ERROR << bitshift | TMP_CRC_DEL_ERROR << bitshift |
+				TMP_CRC_ERROR | TMP_EOF_ERROR << bitshift)) != 0);
+
+			if (has_error) {
 				can.len = 8;
 				can.can_id = get_can_error_id(can.data, header.data_flags, header.data_type == TECMP_DATA_CANFD);
 			}
