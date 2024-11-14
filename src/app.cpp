73b8ac0e8b802e4d1de347e7345d6dd323b1f1e8
Andreas Amereller <andreas.amereller@technica-engineering.de>
2024-11-14 11:28:45 +0100

    Implemented solution proposal.

--- src/app.cpp
+++ src/app.cpp
@@ -18,9 +18,9 @@
 #include "pcap.h"
 #include <args.hxx>
 
-//AnAm: NOTE: Since the proper flag could not be found in the
-// TECMP documentation a temporary variable has been generated
-// to provide a temporary logic.
+// AnAm: NOTE: Since the proper flag could not be found in the
+//  TECMP documentation a temporary variable has been generated
+//  to provide a temporary logic.
 #define TMP_ACK_FLAG 0x0001
 #define TMP_ERROR_NODE_ACTIVE 0x0002
 #define TMP_ERROR_MESSAGE 0x0008
@@ -50,12 +50,14 @@ bool LoadNpcapDlls()
 	TCHAR npcap_dir[512];
 	UINT len;
 	len = GetSystemDirectory(npcap_dir, 480);
-	if (!len) {
+	if (!len)
+	{
 		fprintf(stderr, "Error in GetSystemDirectory: %x", GetLastError());
 		return false;
 	}
 	_tcscat_s(npcap_dir, 512, TEXT("\\Npcap"));
-	if (SetDllDirectory(npcap_dir) == 0) {
+	if (SetDllDirectory(npcap_dir) == 0)
+	{
 		fprintf(stderr, "Error in SetDllDirectory: %x", GetLastError());
 		return false;
 	}
@@ -63,7 +65,8 @@ bool LoadNpcapDlls()
 }
 #endif
 
-static uint32_t get_can_error_id(uint8_t* data, uint16_t flags, bool is_canfd) {
+static uint32_t get_can_error_id(uint8_t *data, uint16_t flags, bool is_canfd)
+{
 	uint32_t can_id = 0x20000000;
 	int bitshift = is_canfd ? 1 : 0;
 	data[2] |= (flags & (TMP_BITSTUFF_ERROR << bitshift)) != 0 ? 0x04 : 0;
@@ -72,7 +75,8 @@ static uint32_t get_can_error_id(uint8_t* data, uint16_t flags, bool is_canfd) {
 	data[3] |= (flags & (TMP_ACK_DEL_ERROR << bitshift)) != 0 ? 0x1B : 0;
 	data[3] |= (flags & (TMP_EOF_ERROR << bitshift)) != 0 ? 0x1A : 0;
 	data[3] |= (flags & TMP_ACK_FLAG) == 0 ? 0x19 : 0;
-	if ((data[2] != 0) || (data[3] != 0)) {
+	if ((data[2] != 0) || (data[3] != 0))
+	{
 		can_id |= 0x00000008;
 	}
 	return can_id;
@@ -84,13 +88,14 @@ void transform(
 	bool drop_replay_data,
 	light_packet_interface packet_interface,
 	light_packet_header packet_header,
-	const uint8_t* packet_data
-) {
+	const uint8_t *packet_data)
+{
 	int32_t iterator = 0;
 	tecmp_header header;
-	uint8_t* data;
+	uint8_t *data;
 	int res = tecmp_next(packet_data, packet_header.captured_length, &iterator, &header, &data);
-	if (res == EINVAL) {
+	if (res == EINVAL)
+	{
 		// not a tecmp packet, copy it as it is
 		if (!tecmp_only)
 		{
@@ -98,15 +103,17 @@ void transform(
 		}
 	}
 	// if we need to drop replay data
-	if (drop_replay_data && header.message_type == TECMP_TYPE_REPLAY_DATA) {
+	if (drop_replay_data && header.message_type == TECMP_TYPE_REPLAY_DATA)
+	{
 		return;
 	}
 	// tecmp packet
-	while (res == 0) {
+	while (res == 0)
+	{
 		packet_interface.timestamp_resolution = NANOS_PER_SEC;
 		packet_header.timestamp = tecmp_get_timespec(header);
 
-		frame_header hdr = { 0 };
+		frame_header hdr = {0};
 		hdr.channel_id = header.channel_id;
 		hdr.timestamp_resolution = packet_interface.timestamp_resolution;
 		hdr.timestamp = packet_header.timestamp;
@@ -116,33 +123,37 @@ void transform(
 
 		// append packet_header info
 		// in case of can, lin or ethernet packets, drop tecmp header
-		if (header.data_type == TECMP_DATA_CAN || header.data_type == TECMP_DATA_CANFD) {
-			struct canfd_frame can = { 0 };
-			can.can_id = ntoh32(*((uint32_t*)data));
+		if (header.data_type == TECMP_DATA_CAN || header.data_type == TECMP_DATA_CANFD)
+		{
+			struct canfd_frame can = {0};
+			can.can_id = ntoh32(*((uint32_t *)data));
 			// Initialize data
 			memset(can.data, 0, sizeof(can.data));
 
 			int bitshift = (header.data_type == TECMP_DATA_CANFD) ? 1 : 0;
 
 			const int errorFlags = TMP_ERROR_MESSAGE |
-				(TMP_BITSTUFF_ERROR << bitshift) |
-				(TMP_ACK_DEL_ERROR << bitshift) |
-				(TMP_CRC_DEL_ERROR << bitshift) |
-				TMP_CRC_ERROR |
-				(TMP_EOF_ERROR << bitshift);
+								   (TMP_BITSTUFF_ERROR << bitshift) |
+								   (TMP_ACK_DEL_ERROR << bitshift) |
+								   (TMP_CRC_DEL_ERROR << bitshift) |
+								   TMP_CRC_ERROR |
+								   (TMP_EOF_ERROR << bitshift);
 
 			bool has_error = (header.data_flags & errorFlags) != 0 ||
-				(header.data_flags & TMP_ACK_FLAG) == 0;
+							 (header.data_flags & TMP_ACK_FLAG) == 0;
 
-			if (has_error) {
+			if (has_error)
+			{
 				can.len = 8;
 				can.can_id = get_can_error_id(can.data, header.data_flags, header.data_type == TECMP_DATA_CANFD);
 			}
-			else {
+			else
+			{
 				can.len = data[4];
 				memcpy(can.data, data + 5, can.len);
 
-				if (header.data_type == TECMP_DATA_CANFD) {
+				if (header.data_type == TECMP_DATA_CANFD)
+				{
 					can.flags |= CANFD_FDF;
 					can.flags |= (header.data_flags & TMP_BITRATE_SWITCH) ? CANFD_BRS : 0;
 					can.flags |= (header.data_flags & TMP_ERROR_NODE_ACTIVE) ? CANFD_ESI : 0;
@@ -156,7 +167,8 @@ void transform(
 			lin_frame lin;
 			lin.pid = data[0];
 			lin.payload_length = len;
-			if (len) {
+			if (len)
+			{
 				memcpy(lin.data, data + 2, len);
 				lin.checksum = data[len + 2];
 			}
@@ -168,27 +180,36 @@ void transform(
 		}
 		else if (header.data_type == TECMP_DATA_ETHERNET)
 		{
-			std::vector<uint8_t>frame(data, data + header.length);
+			std::vector<uint8_t> frame(data, data + header.length);
 			exporter.write_ethernet(hdr, frame);
 		}
 		else if (header.data_type == TECMP_DATA_FLEXRAY)
 		{
 			flexray_frame fr;
 			fr.channel = 0;
-			fr.err_flags = 0;
-			fr.fr_flags =
-				(header.data_flags & 1 ? 0 : FR_NFI) |
-				(header.data_flags & 2 ? FR_STFI : 0) |
-				(header.data_flags & 4 ? FR_SFI : 0) |
-				(header.data_flags & 16 ? FR_PPI : 0);
 
-			fr.cc = data[0];
-			fr.fid = ntoh16(*((uint16_t*)(data + 1)));
-			fr.hcrc = 0;
-			uint8_t len = data[3];
-			fr.len = len / 2;
-			memcpy(fr.data, data + 4, len);
+			// Check for WUS
+			if ((header.data_flags & 0x08) > 0)
+			{
+				fr.type = FR_TYPE_SYMBOL;
+				fr.err_flags = 0x32; // WUS active bits
+			}
+			else
+			{
+				fr.err_flags = 0;
+				fr.fr_flags =
+					(header.data_flags & 1 ? 0 : FR_NFI) |
+					(header.data_flags & 2 ? FR_STFI : 0) |
+					(header.data_flags & 4 ? FR_SFI : 0) |
+					(header.data_flags & 16 ? FR_PPI : 0);
 
+				fr.cc = data[0];
+				fr.fid = ntoh16(*((uint16_t *)(data + 1)));
+				fr.hcrc = 0;
+				uint8_t len = data[3];
+				fr.len = len / 2;
+				memcpy(fr.data, data + 4, len);
+			}
 			exporter.write_flexray(hdr, fr);
 		}
 		else
@@ -196,11 +217,11 @@ void transform(
 			exporter.write_packet(header.channel_id, packet_interface, packet_header, packet_data);
 		}
 		res = tecmp_next(packet_data, packet_header.captured_length, &iterator, &header, &data);
-
 	}
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
 #if _WIN32
 	LoadNpcapDlls();
@@ -210,13 +231,13 @@ int main(int argc, char* argv[]) {
 	parser.helpParams.showTerminator = false;
 	parser.helpParams.proglineShowFlags = true;
 
-	args::HelpFlag help(parser, "help", "", { 'h', "help" }, args::Options::HiddenFromUsage);
-	args::ValueFlag<std::string> maparg(parser, "map-file", "Configuration file for channel mapping", { "channel-map" });
+	args::HelpFlag help(parser, "help", "", {'h', "help"}, args::Options::HiddenFromUsage);
+	args::ValueFlag<std::string> maparg(parser, "map-file", "Configuration file for channel mapping", {"channel-map"});
 
 	args::Positional<std::string> inarg(parser, "infile", "Input File", args::Options::Required);
 	args::Positional<std::string> outarg(parser, "outfile", "Output File", args::Options::Required);
-	args::Flag tecmp_only(parser, "tecmp-only", "Only process TECMP packets, drop others", { "tecmp-only" }, args::Options::Single);
-	args::Flag drop_replay_data(parser, "drop-replay-data", "Drop replay data messages", { "drop-replay-data" }, args::Options::Single);
+	args::Flag tecmp_only(parser, "tecmp-only", "Only process TECMP packets, drop others", {"tecmp-only"}, args::Options::Single);
+	args::Flag drop_replay_data(parser, "drop-replay-data", "Drop replay data messages", {"drop-replay-data"}, args::Options::Single);
 
 	try
 	{
@@ -237,44 +258,50 @@ int main(int argc, char* argv[]) {
 	int magic = 0;
 	std::ifstream file_check;
 	file_check.open(args::get(inarg), std::ios::binary | std::ios::in);
-	file_check.read((char*)&magic, 4);
+	file_check.read((char *)&magic, 4);
 	file_check.close();
 
 	PcapngExporter exporter = PcapngExporter(args::get(outarg), maparg.Get());
 
-	if (magic == PCAP_NG_MAGIC_NUMBER) {
+	if (magic == PCAP_NG_MAGIC_NUMBER)
+	{
 		light_pcapng infile = light_pcapng_open(args::get(inarg).c_str(), "rb");
-		if (!infile) {
+		if (!infile)
+		{
 			std::cerr << "Unable to open: " << args::get(inarg) << std::endl;
 			return 1;
 		}
 
-		// the packet 
-		light_packet_interface packet_interface = { 0 };
-		light_packet_header packet_header = { 0 };
-		const uint8_t* packet_data = nullptr;
+		// the packet
+		light_packet_interface packet_interface = {0};
+		light_packet_header packet_header = {0};
+		const uint8_t *packet_data = nullptr;
 
-		while (light_read_packet(infile, &packet_interface, &packet_header, &packet_data)) {
+		while (light_read_packet(infile, &packet_interface, &packet_header, &packet_data))
+		{
 			transform(exporter, tecmp_only.Get(), drop_replay_data.Get(), packet_interface, packet_header, packet_data);
 		}
 
 		light_pcapng_close(infile);
 	}
-	else if (magic == PCAP_MAGIC_NUMBER || magic == PCAP_MAGIC_NUMBER_LITTLE_ENDIAN) {
+	else if (magic == PCAP_MAGIC_NUMBER || magic == PCAP_MAGIC_NUMBER_LITTLE_ENDIAN)
+	{
 		char errbuf[PCAP_ERRBUF_SIZE];
-		pcap_t* infile = pcap_open_offline(args::get(inarg).c_str(), errbuf);
+		pcap_t *infile = pcap_open_offline(args::get(inarg).c_str(), errbuf);
 		int link_layer = pcap_datalink(infile);
-		if (!infile) {
+		if (!infile)
+		{
 			std::cerr << "Unable to open: " << args::get(inarg) << std::endl;
 			return 1;
 		}
 		// the packet
 		pcap_pkthdr pkthdr;
-		const uint8_t* packet_data = pcap_next(infile, &pkthdr);
-		while (packet_data) {
+		const uint8_t *packet_data = pcap_next(infile, &pkthdr);
+		while (packet_data)
+		{
 			// light pcapng packet
-			light_packet_interface packet_interface = { 0 };
-			light_packet_header packet_header = { 0 };
+			light_packet_interface packet_interface = {0};
+			light_packet_header packet_header = {0};
 			packet_interface.link_type = link_layer;
 			packet_header.captured_length = pkthdr.caplen;
 			packet_header.original_length = pkthdr.len;
@@ -286,7 +313,8 @@ int main(int argc, char* argv[]) {
 			packet_data = pcap_next(infile, &pkthdr);
 		}
 	}
-	else {
+	else
+	{
 		exporter.close();
 		std::remove(args::get(outarg).c_str());
 		std::cerr << "Not valid input file: " << args::get(inarg) << std::endl;
